988ce49d03eff2a024b1a889d23685c61aa9dcca
Sudhansu Bhushan Mishra <13695177+the-lazy-learner@users.noreply.github.com>
2023-06-16 16:42:16 +0530

    Add individual errors for each field.

--- forms-validation-blazor/BlazingPizza/Models/Address.cs
+++ forms-validation-blazor/BlazingPizza/Models/Address.cs
@@ -6,21 +6,21 @@ public class Address
 {
     public int Id { get; set; }
 
-    [Required, MinLength(3), MaxLength(100)]
+    [Required, MinLength(3, ErrorMessage = "Please use a name longer than 3 letters."), MaxLength(100, ErrorMessage = "Please use a name fewer than 100 letters.")]
     public string? Name { get; set; }
 
-    [Required, MinLength(5), MaxLength(100)]
+    [Required, MinLength(5, ErrorMessage = "Please use an Address line longer than 5 letters."), MaxLength(100, ErrorMessage = "Please use an Address Line fewer than 100 letters.")]
     public string? Line1 { get; set; }
 
-    [MaxLength(100)]
+    [MaxLength(100, ErrorMessage = "Please use an Address line fewer than 100 letters.")]
     public string? Line2 { get; set; }
 
-    [Required, MinLength(3), MaxLength(50)]
+    [Required, MinLength(3, ErrorMessage = "Please use a city name longer than 3 letters."), MaxLength(50, ErrorMessage = "Please use a city name fewer than 50 letters.")]
     public string? City { get; set; }
 
-    [Required, MinLength(3), MaxLength(20)]
+    [Required, MinLength(3, ErrorMessage = "Please use a region longer than 3 letters."), MaxLength(20, ErrorMessage = "Please use a region fewer than 20 letters.")]
     public string? Region { get; set; }
 
-    [Required, RegularExpression(@"^([0-9]{6})$")]
+    [Required, RegularExpression(@"^([0-9]{5-6})$", ErrorMessage = "Please use a valid postal code with 5 or 6 digits.")]
     public string? PostalCode { get; set; }
 }
