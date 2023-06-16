adeccbf31302650ae2d5dc52a79511261c05a351
Sudhansu Bhushan Mishra <13695177+the-lazy-learner@users.noreply.github.com>
2023-06-16 15:48:11 +0530

    Add validation using ValidationSummary and DataAnnotationsValidator

--- forms-validation-blazor/BlazingPizza/Models/Address.cs
+++ forms-validation-blazor/BlazingPizza/Models/Address.cs
@@ -1,18 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BlazingPizza;
 
 public class Address
 {
     public int Id { get; set; }
 
+    [Required, MinLength(3), MaxLength(100)]
     public string? Name { get; set; }
 
+    [Required, MinLength(5), MaxLength(100)]
     public string? Line1 { get; set; }
 
+    [MaxLength(100)]
     public string? Line2 { get; set; }
 
+    [Required, MinLength(3), MaxLength(50)]
     public string? City { get; set; }
 
+    [Required, MinLength(3), MaxLength(20)]
     public string? Region { get; set; }
 
+    [Required, RegularExpression(@"^([0-9]{6})$")]
     public string? PostalCode { get; set; }
 }
