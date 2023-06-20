e4f83ee6cb1de59fcc697f8b4eeb7d32796d62db
Sudhansu Bhushan Mishra <13695177+the-lazy-learner@users.noreply.github.com>
2023-06-20 16:05:27 +0530

    Use SQlite instead of in memory for the same functionality

--- minimal-api-ef-core/PizzaStore/Program.cs
+++ minimal-api-ef-core/PizzaStore/Program.cs
@@ -5,9 +5,12 @@ using PizzaStore.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = builder.Configuration.GetConnectionString("Pizzas") ?? "Data Source=Pizzas.db";
+
 builder.Services.AddEndpointsApiExplorer();
 
-builder.Services.AddDbContext<PizzaDb>(options => options.UseInMemoryDatabase("items"));
+// builder.Services.AddDbContext<PizzaDb>(options => options.UseInMemoryDatabase("items"));
+builder.Services.AddSqlite<PizzaDb>(connectionString);
 
 builder.Services.AddSwaggerGen(c =>
 {
